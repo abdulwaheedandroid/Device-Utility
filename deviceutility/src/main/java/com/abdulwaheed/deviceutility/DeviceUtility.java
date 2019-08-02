731b1cadd7e7b36824dab776ee0e13dd31124ab1
Abdul Waheed <bawan@abacusoft.com>
2019-08-02 21:33:17 +0500

    Added device width and height method

--- deviceutility/src/main/java/com/abdulwaheed/deviceutility/DeviceUtility.java
+++ deviceutility/src/main/java/com/abdulwaheed/deviceutility/DeviceUtility.java
@@ -1,8 +1,18 @@
 package com.abdulwaheed.deviceutility;
 
+import android.content.res.Resources;
+
 public class DeviceUtility {
 
     public static boolean isDeviceTablet () {
         return false;
+    }
+
+    public static int getDeviceHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    public static int getDeviceWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
 }
